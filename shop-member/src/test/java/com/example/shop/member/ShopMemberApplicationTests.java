db812776cc5f43b64f0eef2f4776a09774ec1158
shi182138 <shi182138@163.com>
2022-08-04 15:38:41 +0800

    项目初始结构创建1

---
+++ shop-member/src/test/java/com/example/shop/member/ShopMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shop-ware/src/test/java/com/example/shop/ware/ShopWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
